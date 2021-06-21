f3c635d16d73848b3076f1509e3b6509e142a74a
Martin Erpicum <martin@erpicum.net>
2021-06-21 16:02:33 +0200

    Update CollectionCodebookController.php

--- src/app/Http/Controllers/Collection/CollectionCodebookController.php
+++ src/app/Http/Controllers/Collection/CollectionCodebookController.php
@@ -41,16 +41,49 @@ class CollectionCodebookController extends Controller
         $section = $phpWord->addSection();
 
         // Define styles
-        $fontStyle12 = array('spaceAfter' => 60, 'size' => 12);
-        $fontStyle10 = array('size' => 10);
+        $fontStyle12 = ['spaceAfter' => 60, 'size' => 12];
+        $fontStyle10 = ['size' => 10];
 
         $phpWord->addTitleStyle(null, ['size' => 22, 'bold' => true]);
-        $phpWord->addTitleStyle(1, ['size' => 20, 'color' => '0171c5', 'bold' => true]);
-        $phpWord->addTitleStyle(2, ['size' => 16, 'color' => '666666', 'bold' => true, 'underline' => 'single']);
+        $phpWord->addTitleStyle(1, ['size' => 20, 'color' => '#0171c5', 'bold' => true]);
+        $phpWord->addTitleStyle(2, ['size' => 16, 'color' => '#666666', 'bold' => true, 'underline' => 'single']);
         $phpWord->addTitleStyle(3, ['size' => 14, 'italic' => true]);
         $phpWord->addTitleStyle(4, ['size' => 12]);
 
         $section->addTitle($collection->name, 0);
+
+        $section->addText($collection->description);
+
+        $section->addText('Exported from Corpus at ' . now());
+
+
+        $section->addTitle('Texts from ' . $collection->name, 1);
+
+        $tableStyle = [
+            'borderColor' => '#cccccc',
+            'borderSize'  => 6,
+            'cellMargin'  => 50
+        ];
+        $firstRowStyle = ['bgColor' => '#344c77', 'color' => '#FFFFFF', 'bold' => true];
+        $phpWord->addTableStyle('myTable', $tableStyle, $firstRowStyle);
+        $table = $section->addTable('myTable');
+        $table->addRow();
+        $table->addCell()->addText('#');
+        $table->addCell()->addText('Title');
+        $table->addCell()->addText('Abstract');
+        $table->addCell()->addText('Reading estimate');
+        $table->addCell()->addText('Tagging estimate');
+        $table->addCell()->addText('Updated');
+
+        foreach ($collection->texts as $key => $text) {
+            $table->addRow();
+            $table->addCell()->addText($key + 1);
+            $table->addCell()->addText($text->name);
+            $table->addCell()->addText($text->abstract);
+            $table->addCell()->addText($text->stats['reading_estimate']['value']);
+            $table->addCell()->addText($text->stats['tagging_estimate']['value']);
+            $table->addCell()->addText($text->updated_at);
+        }
 
         $section->addPageBreak();
         // Add text elements
@@ -60,35 +93,28 @@ class CollectionCodebookController extends Controller
         // Add TOC
         $toc = $section->addTOC($fontStyle12);
         $section->addTextBreak(2);
+    
 
         $section = $phpWord->addSection();
-
-        $section->addTitle($collection->name, 1);
-        $section->addText($collection->description);
-
-        $section->addText(now());
-
-        $section->addTitle('List of texts from Corpus ' . $collection->name, 1);
-        foreach ($collection->texts as $key => $text) {
-            $section->addTitle($text->name, 2);
-            $section->addTextBreak(1);
-        }
-
-        $section->addTitle('List of tags from Corpus ' . $collection->name, 1);
+        $section->addTitle('Codebook of ' . $collection->name, 1);
         foreach ($collection->tags as $key => $tag) {
             $section->addTitle($tag->name, 2);
             foreach ($tag->snippets as $snippet) {
-                $section->addText($snippet['snippet_content']);
-                $section->addTextBreak(1);
-                $section->addText($snippet['text']['name'], ['size' => 8, 'color' => 'ccc', 'underline' => 'single', 'rtl' => 'right' ]);
-                $section->addTextBreak(2);
+                $textrun = $section->addTextRun();
+
+                $before_snippet = mb_substr($snippet['segment_content'], $snippet['snippet_start'] <= 50 ? 0 : $snippet['snippet_start'] - 50, $snippet['snippet_start'] <= 50 ? $snippet['snippet_start'] : 50);
+                $after_snippet = mb_substr($snippet['segment_content'], $snippet['snippet_end'], $snippet['snippet_end'] + 50 >= mb_strlen($snippet['segment_content']) ? mb_strlen($snippet['segment_content']) - $snippet['snippet_end'] : 50);
+                $textrun->addText(' [...] ');
+                $textrun->addText($before_snippet);
+                $textrun->addText($snippet['snippet_content'], ['bgColor' => '#FFFF00']);
+                $textrun->addText($after_snippet);
+                $textrun->addText(' [...] ');
+                $textrun->addTextBreak(1);
+                $textrun->addText($snippet['text']['name'], ['size' => 8, 'color' => '#666666', 'underline' => 'single']);
+                $textrun->addTextBreak(2);
             }
         }
         
-        // $section->addText('Lead text.');
-        // $footnote = $section->addFootnote();
-        // $footnote->addText('Footnote text.');
-
         // Saving the document as OOXML file...
         $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
 
